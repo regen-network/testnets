ca1819c9b5a1c298896c17b8b343ad45b4f8cbb6
DimAn <71597545+diman-io@users.noreply.github.com>
2021-01-30 19:57:10 +0300

    remove source

--- scripts/gen_val_keys.sh
+++ scripts/gen_val_keys.sh
@@ -21,8 +21,12 @@ else
   echo 'export GOBIN=$GOPATH/bin' >> ~/.bashrc
   echo 'export PATH=$PATH:/usr/local/go/bin:$GOBIN' >> ~/.bashrc
 
-  source ~/.bashrc
+  #source ~/.bashrc
   #. ~/.bashrc
+  export GOPATH=$HOME/go
+  export GOROOT=/usr/local/go
+  export GOBIN=$GOPATH/bin
+  export PATH=$PATH:/usr/local/go/bin:$GOBIN
 
   go version
 fi
