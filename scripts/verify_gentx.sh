3aeac42e3ba0c79ed1c29f13cc6371e8730d9eab
Anil Kumar Kammari <anil@vitwit.com>
2021-02-05 01:17:31 +0530

    Fix gentx github action (#392)
    * read only 1 gentx file
    
    * Fix paths
    
    * Fix gentx validation
    
    * Compile from source
    
    * fix path
    
    * add listing
    
    * add ls
    
    * Fix path hopefully
    
    * fix release version

--- scripts/verify_gentx.sh
+++ scripts/verify_gentx.sh
@@ -3,11 +3,11 @@ REGEN_HOME="/tmp/regen$(date +%s)"
 RANDOM_KEY="randomregenvalidatorkey"
 CHAIN_ID=aplikigo-1
 
-GENTX_FILE=$(find ./aplikigo-1/gentxs -iname "*.json")
+GENTX_FILE=$(find ./$CHAIN_ID/gentxs -iname "*.json")
 LEN_GENTX=$(echo ${#GENTX_FILE})
 
-GENTX_STARTDATE=$(date -d '05-02-2021 15:00:00' '+%d/%m/%Y %H:%M:%S')
-GENTX_DEADLINE=$(date -d '07-02-2021 15:00:00' '+%d/%m/%Y %H:%M:%S')
+# GENTX_STARTDATE=$(date -d '05-02-2021 15:00:00' '+%d/%m/%Y %H:%M:%S')
+# GENTX_DEADLINE=$(date -d '07-02-2021 15:00:00' '+%d/%m/%Y %H:%M:%S')
 now=$(date +"%d/%m/%Y %H:%M:%S")
 
 # if [ $now -le $GENTX_STARTDATE ]; then
@@ -25,16 +25,25 @@ if [ $LEN_GENTX -eq 0 ]; then
 else
     set -e
 
-    ./scripts/check-gentx-amount.sh "./$CHAIN_ID/gentxs/$GENTX_FILE" || exit 1
+    echo "GentxFile::::"
+    echo $GENTX_FILE
+
+    ./scripts/check-gentx-amount.sh "$GENTX_FILE" || exit 1
 
     echo "...........Init Regen.............."
-    curl -L https://github.com/regen-network/regen-ledger/releases/download/v0.6.0-alpha6/regen_0.6.0_linux_amd64.zip -o regen_linux.zip && unzip regen_linux.zip
-    rm regen_linux.zip
+    # wget https://github.com/regen-network/regen-ledger/releases/download/v0.6.0-alpha6/regen_0.6.0_linux_arm64.tar.gz && tar -xzvf regen_0.6.0_linux_arm64.tar.gz
+    # rm regen_0.6.0_linux_arm64.tar.gz
     #cd regen_0.6.0_linux_amd64
 
-    ./regen keys add $RANDOM_KEY --keyring-backend test --home $REGEN_HOME
+    git clone https://github.com/regen-network/regen-ledger
+    cd regen-ledger
+    git checkout v0.6.0
+    make build
+    chmod +x ./build/regen
 
-    ./regen init --chain-id $CHAIN_ID validator --home $REGEN_HOME
+    ./build/regen keys add $RANDOM_KEY --keyring-backend test --home $REGEN_HOME
+
+    ./build/regen init --chain-id $CHAIN_ID validator --home $REGEN_HOME
 
     echo "..........Fetching genesis......."
     rm -rf $REGEN_HOME/config/genesis.json
@@ -42,32 +51,32 @@ else
 
     sed -i '/genesis_time/c\   \"genesis_time\" : \"2021-01-01T00:00:00Z\",' $REGEN_HOME/config/genesis.json
 
-    GENACC=$(cat ../$CHAIN_ID/gentxs/$GENTX_FILE | sed -n 's|.*"delegator_address":"\([^"]*\)".*|\1|p')
+    GENACC=$(cat ../$GENTX_FILE | sed -n 's|.*"delegator_address":"\([^"]*\)".*|\1|p')
 
     echo $GENACC
 
-    ./regen add-genesis-account $RANDOM_KEY 1000000000000utree --home $REGEN_HOME \
+    ./build/regen add-genesis-account $RANDOM_KEY 1000000000000utree --home $REGEN_HOME \
         --keyring-backend test
-    ./regen add-genesis-account $GENACC 100000000000utree --home $REGEN_HOME
+    ./build/regen add-genesis-account $GENACC 100000000000utree --home $REGEN_HOME
 
-    ./regen gentx --name $RANDOM_KEY --amount 900000000000utree --home $REGEN_HOME \
-        --keyring-backend test
-    cp ../$CHAIN_ID/gentxs/$GENTX_FILE $REGEN_HOME/config/gentx/
+    ./build/regen gentx $RANDOM_KEY 900000000000utree --home $REGEN_HOME \
+        --keyring-backend test --chain-id $CHAIN_ID
+    cp ../$GENTX_FILE $REGEN_HOME/config/gentx/
 
     echo "..........Collecting gentxs......."
-    ./regen collect-gentxs --home $REGEN_HOME
+    ./build/regen collect-gentxs --home $REGEN_HOME
     sed -i '/persistent_peers =/c\persistent_peers = ""' $REGEN_HOME/config/config.toml
 
-    ./regen validate-genesis --home $REGEN_HOME
+    ./build/regen validate-genesis --home $REGEN_HOME
 
     echo "..........Starting node......."
-    ./regen start --home $REGEN_HOME &
+    ./build/regen start --home $REGEN_HOME &
 
     sleep 5s
 
     echo "...checking network status.."
 
-    ./regen status --chain-id $CHAIN_ID --node http://localhost:26657
+    ./build/regen status --node http://localhost:26657
 
     echo "...Cleaning the stuff..."
     killall regen >/dev/null 2>&1
